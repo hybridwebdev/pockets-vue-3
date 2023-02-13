255043f12aeced25d64be96dbd8a2037fab97257
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-13 05:56:37 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/editor/modes/copy/index.vue
+++ plugins/pockets/vue/dev/src/node-tree/editor/modes/copy/index.vue
@@ -1,36 +1,53 @@
 <template>
-    <div v-if='editor.selectedNodes.length==0'>    
-        <button @click='trigger'>
-            Copy Element
-        </button>
-    </div>
+    <div class='p-2'>
+        <div v-if='editor.selectedNodes.length==0'>    
+            <button @click='trigger' class='btn btn-accent-dk p-2'>
+                Copy Element
+            </button>
+        </div>
 
-    <div v-if='!editor.selectedNodes.length==0'>    
-        <nodePlacement 
-            :api='editor.active'
-            @before='confirm("before")'
-            @after='confirm("after")'
-            @inside='confirm("inside")'
-        />
-        <button @click='confirm'>
-            Confirm Placement
-        </button>
+        <div v-if='!editor.selectedNodes.length==0' class='grid columns-3 gap-1'>    
+            <nodePlacement 
+                :api='editor.active'
+                @before='confirm("before")'
+                @after='confirm("after")'
+                @inside='confirm("inside")'
+            />
+        </div>
     </div>
-
 </template>
 <script lang='ts' setup>
 import nodePlacement from "@/node-tree/editor/sections/node-placement"
-import { inject } from "vue"
+import { inject, toRaw  } from "vue"
 
 let editor = inject('pockets-node-tree-editor')
 
 let trigger = () => {
     editor.selectNode(editor.active.activateInfo)
-    // editor.setActiveNode(false)
-    console.log(editor.selectedNodes)
 }
+let selectedNode = $computed(() => {
+    return editor.selectedNodes[0] ?? false
+})
+let confirm = async (location) => {
+    let node = toRaw (selectedNode.node)
+    let map = {
+        inside: async () => {
+            console.log(node)
+            // selectedNode.remove()
+            // editor.active.addSibling(selectedNode.node, false)
+        },
+        before: async () => {
+            console.log(node)
+            // selectedNode.remove()
+            // editor.active.addSibling(selectedNode.node, false)
+        },
+    }
 
-let confirm = (location) => {
+    let fn = map[location] ?? false
+    if(typeof fn != 'function') return
+
+    await fn()
+    console.log('off')
     editor.selectNode(false)
 }
 </script>

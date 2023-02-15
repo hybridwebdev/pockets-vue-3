10afd7d3c8752a604e085775a04141df6a2e666d
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-15 02:47:16 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/root/node-tree-api/remove.ts
+++ plugins/pockets/vue/dev/src/node-tree/root/node-tree-api/remove.ts
@@ -1,11 +1,8 @@
 import { $pockets } from "@/pockets"
-import type { remove } from "./types"
+import type { remove, TreeNodeApi } from "./types"
 
-export let useRemove = (api) => {
-    let remove:remove = {
-        self: false,
-        node: false
-    }
+export let useRemove = (api:TreeNodeApi) : remove => {
+   
     let node = (index: number) => {
         api.node.nodes = $pockets.utils.array.omit(api.node.nodes, index)
         return []
@@ -13,10 +10,14 @@ export let useRemove = (api) => {
     let self = () => {
         return api.parent.remove.node(api.index)
     }
-    if(api.parent) {
+    let remove:remove = {
+        self,
+        node
+    }
+    if(!api.parent) {
         remove.self = self
     }
-    if(api.node.nodes) {
+    if(!api.node.nodes) {
         remove.node = node
     }
     return remove

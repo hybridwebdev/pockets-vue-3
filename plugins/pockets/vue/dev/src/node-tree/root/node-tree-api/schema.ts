30eb72b4c45354c65d6c21998eaeb87fc99b4da4
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-15 02:48:27 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/root/node-tree-api/schema.ts
+++ plugins/pockets/vue/dev/src/node-tree/root/node-tree-api/schema.ts
@@ -1,6 +1,6 @@
-import { TreeNodeSchema } from "./types"
+import { TreeNodeSchema, TreeNodeApi } from "./types"
 
-export let useSchema = (api) : TreeNodeSchema | false => {
+export let useSchema = (api : TreeNodeApi) : TreeNodeSchema | false => {
     return api.editor.nodes.list.find(entry => {
         return entry.node.schema == api.node.schema
     } ) ?? false

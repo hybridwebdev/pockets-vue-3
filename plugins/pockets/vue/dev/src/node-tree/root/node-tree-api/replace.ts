0f05ff079c2977cd87d15d4cc817b55353252315
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-15 04:55:12 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/root/node-tree-api/replace.ts
+++ plugins/pockets/vue/dev/src/node-tree/root/node-tree-api/replace.ts
@@ -1,9 +1,10 @@
 import { TreeNodeApi, replace } from "./types";
+import { $pockets } from "@/pockets"
 
 export let useReplace = (api: TreeNodeApi) : replace =>{
     let replace:replace = {
         child(index: number, node){
-            api.node.nodes[index] = node
+            $pockets.utils.object.set(api.node.nodes, index, node)
         },
         self(node){
             api.parent.replace.child(api.index, node)
--- plugins/pockets/vue/dev/src/pockets/utils/dot-prop/object.ts
+++ plugins/pockets/vue/dev/src/pockets/utils/dot-prop/object.ts
@@ -1,16 +1,5 @@
-import { get } from "lodash"
-
-let set = (path: string, value: any, obj: any) => {
-
-    var parts = path.split("."), p=parts.pop() 
-    
-    for(var i=0, j; obj && ( j = parts[i] ); i++ ) {
-        obj = (j in obj ? obj[j] : obj[j]={} ) 
-    }
-
-    return obj && p ? (obj[p]=value) : undefined 
-}
-
+import { get, set } from "lodash"
+ 
 //let get = ( path: string, obj: any ) => path.split('.').reduce( ( o: any, i: string ) => o[i], obj )
 
 export let clone = (o: object) => JSON.parse( JSON.stringify( o ) )

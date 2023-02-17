7be8bfea60b410e8c754f08ab3339a820e257d16
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-17 04:52:22 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/root/index.vue
+++ plugins/pockets/vue/dev/src/node-tree/root/index.vue
@@ -6,8 +6,11 @@
 </template>
 <script lang='ts' setup>
 import treeWalker from "@/node-tree/walker"
-import { provide } from "vue"
-import { createApi } from './node-tree-api'
+
+import { provide, InjectionKey } from "vue"
+
+import { createApi } from '@/node-tree/node-tree-api'
+
 let props = defineProps( {
     root: {
         type: Object,
@@ -23,6 +26,8 @@ provide('pockets-node-tree-root', props )
 let api = createApi(props)
 
 provide('pockets-node-tree-api', api )
+
+// export let API_INJECTION_KEY: InjectionKey<> = Symbol('pockets-node-tree-api')
 
 
 </script>

76bbb4a8cb26bf938fdbeb3e36dbd54b7c1db8e4
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-17 22:49:26 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/create-tree-api/edit-fields.ts
+++ plugins/pockets/vue/dev/src/node-tree/create-tree-api/edit-fields.ts
@@ -1,10 +1,10 @@
 import { TreeNodeApi,  TreeNodeSchemaField } from "@/node-tree/types"
-import { editor } from "@/node-tree/editor"
+
 export let useEditFields = (api:TreeNodeApi) : Array<TreeNodeSchemaField> => {
     
-    if(!api.schema || !api.node || !editor.nodes.fields) return []
+    if(!api.schema || !api.node || !api.editor.nodes.fields) return []
     
-    return editor.nodes.fields.filter((field: TreeNodeSchemaField) => {
+    return api.editor.nodes.fields.filter((field: TreeNodeSchemaField) => {
         return api.schema.fields.includes(field.ID)
     })
 
--- plugins/pockets/vue/dev/src/node-tree/create-tree-api/schema.ts
+++ plugins/pockets/vue/dev/src/node-tree/create-tree-api/schema.ts
@@ -1,7 +1,7 @@
 import { TreeNodeSchema, TreeNodeApi } from "@/node-tree/types"
-import { editor } from "@/node-tree/editor"
+
 export let useSchema = (api : TreeNodeApi) : TreeNodeSchema | false => {
-    return editor.nodes.list.find((entry: TreeNodeSchema) => {
+    return api.editor.nodes.list.find((entry: TreeNodeSchema) => {
         return entry.node.schema == api.node.schema
     } ) ?? false
 }

0a74c18e2a57b45e057a244e1c272557e04cf1c0
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-18 06:21:09 -0600

    c

--- plugins/pockets/vue/dev/src/pockets/utils/dot-prop/object.ts
+++ plugins/pockets/vue/dev/src/pockets/utils/dot-prop/object.ts
@@ -1,7 +1,5 @@
 import { get, set } from "lodash"
  
-//let get = ( path: string, obj: any ) => path.split('.').reduce( ( o: any, i: string ) => o[i], obj )
-
 export let clone = (o: object) => JSON.parse( JSON.stringify( o ) )
 
 export let $object = {

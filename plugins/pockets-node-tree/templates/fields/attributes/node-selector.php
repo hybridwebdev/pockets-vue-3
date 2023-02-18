aae9bddc4b4f984aa72a8223a027dfa18ead469b
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-18 04:34:22 -0600

    c

--- plugins/pockets-node-tree/templates/fields/attributes/node-selector.php
+++ plugins/pockets-node-tree/templates/fields/attributes/node-selector.php
@@ -1,4 +1,9 @@
-<div v-if='initialize.self'>
+<div v-if='!parent' class='text-danger fw-8 fs-20 pt-2'>
+    <p class='text-center'>
+        This Node cannot be changed!
+    </p>
+</div>
+<div v-if='parent'>
     <label>Node Schema</label>
     <pockets-node-tree-schema-selector 
         v-model:selected='node.schema' 

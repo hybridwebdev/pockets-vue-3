54dc4948a567225ac054c8095610d0c4657b612f
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-22 04:16:54 -0600

    c

---
+++ plugins/pockets/vue/dev/src/node-tree/editor/modes/copy/move.ts
@@ -0,0 +1,97 @@
+import type { TreeNodeApi } from "@/node-tree/types"
+
+type api = {
+    inside: number | boolean | (() => any)
+    before: number | boolean | (() => any)
+    after : number | boolean | (() => any)
+}
+
+export let move = ( active: TreeNodeApi | false, selected: false | TreeNodeApi ) : api => {
+
+    let invalid:api = {
+        inside: false,
+        before: false,
+        after: false
+    }
+
+    if(!active || !selected)  return invalid
+
+    let indexes = {
+        active: active.paths.index,
+        selected: selected.paths.index
+    }
+
+    let sameParent = () => {
+        if(!selected.parent || !active.parent) return;
+        return selected.parent.paths.full == active.parent.paths.full
+    }
+
+    let sameIndex = () => indexes.active == indexes.selected
+
+    let moveFn = (index: number) => {
+        return index
+        //return () => selected.move.self(index)
+    }
+    let before = () => {
+
+        if( sameParent() === true ){
+    
+            if( sameIndex() ) return false;
+
+            if( indexes.selected+1 == indexes.active) {
+                /**
+                    if the item left of it is the active 
+                    target then it can't move
+                */
+                return false;
+            }
+
+            if(indexes.active > indexes.selected) {
+                /*
+                    nudging it up one so that it doesn't 
+                    replace its sibling
+                */
+                return moveFn(indexes.active--)
+            }
+
+            return moveFn(indexes.active)
+
+        }
+
+        return false
+
+    }
+    
+    let after = () => {
+
+        if( sameParent() === true ) {
+            
+            if( sameIndex() ) return false;
+
+            if(indexes.selected-1 == indexes.active)  {
+                 /**
+                    if the item right of it is the active 
+                    target then it can't move
+                */
+                return false;
+            }
+        
+            return moveFn(indexes.active)
+
+        }
+
+        return false;
+    }
+
+    let inside = () => {
+        return false
+    }
+
+    let api:api = {
+        before: before(),
+        after: after(),
+        inside: inside()
+    }
+
+    return api
+}
--- plugins/pockets/vue/dev/src/node-tree/editor/modes/copy/setup.ts
+++ plugins/pockets/vue/dev/src/node-tree/editor/modes/copy/setup.ts
@@ -1,8 +1,6 @@
 import { reactive, computed, toRefs, ref, toRef, Ref } from "vue"
 import { editor } from "@/node-tree/editor"
-
-import type { TreeNodeApi } from "@/node-tree/types"
-
+import { move } from "./move"
 export let setup = (props) => {
    
     let hasSameParent = computed(() => {
@@ -79,107 +77,9 @@ export let setup = (props) => {
         cancel,
         hasSameParent,
         indexes,
-        abstract: computed(() => abstract( editor.active, selected.value ))
+        abstract: computed(() => move( editor.active, selected.value ))
     } )
 
     return api
 
-}
-
-
-let abstract = ( active: TreeNodeApi | false, selected: false | TreeNodeApi ) => {
-    
-    type api = {
-        inside: number | boolean | (() => any)
-        before: number | boolean | (() => any)
-        after : number | boolean | (() => any)
-    }
-
-    let invalid:api = {
-        inside: false,
-        before: false,
-        after: false
-    }
-
-    if(!active || !selected)  return invalid
-
-    let indexes = {
-        active: active.paths.index,
-        selected: selected.paths.index
-    }
-
-    let sameParent = () => {
-        if(!selected.parent || !active.parent) return;
-        return selected.parent.paths.full == active.parent.paths.full
-    }
-
-    let sameIndex = () => indexes.active == indexes.selected
-
-    let moveFn = (index: number) => {
-        return index
-        //return () => selected.move.self(index)
-    }
-    let before = () => {
-
-        if( sameParent() === true ){
-    
-            if( sameIndex() ) return false;
-
-            if( indexes.selected+1 == indexes.active) {
-                /**
-                    if the item left of it is the active 
-                    target then it can't move
-                */
-                return false;
-            }
-
-            if(indexes.active > indexes.selected) {
-                /*
-                    nudging it up one so that it doesn't 
-                    replace its sibling
-                */
-                return moveFn(indexes.active--)
-            }
-
-            return moveFn(indexes.active)
-
-        }
-
-        return false
-
-    }
-    
-    let after = () => {
-
-        if( sameParent() === true ) {
-            
-            if( sameIndex() ) return false;
-
-            if(indexes.selected-1 == indexes.active)  {
-                 /**
-                    if the item right of it is the active 
-                    target then it can't move
-                */
-                return false;
-            }
-        
-            return moveFn(indexes.active)
-
-        }
-
-        return false;
-    }
-
-    let inside = () => {
-        return false
-        // return 'hello world'
-    }
-
-    let api:api = {
-        before: before(),
-        after: after(),
-        inside: inside()
-    }
-
-    return api
 }

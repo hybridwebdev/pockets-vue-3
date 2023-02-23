6200efb6bbcd3afda6e5466de95765db9dcf35b6
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-23 05:14:42 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/tests/unit/basic-operations.spec.ts
+++ plugins/pockets/vue/dev/src/node-tree/tests/unit/basic-operations.spec.ts
@@ -1,7 +1,6 @@
 import { node, getTree } from "./"
 
-let cloneSelf = () => {
-
+let clone = () => {
     let tree = getTree()
     
     tree.getNode(['root', 0, 2]).clone.self()
@@ -9,23 +8,22 @@ let cloneSelf = () => {
     expect(
         tree.getNode(['root', 0, 2]).node.props.key
     ).toBe('root.0.2')
-
 }
 
-let removeSelf = () => {
-    let tree = getTree()
+let remove = () => {
     
+    let tree = getTree()
     tree.getNode(['root', 0, 0]).remove.self()
-
     expect(
         tree.getNode(['root', 0, 0]).node.props.key
     ).toBe('root.0.1')
+
 }
 
 let addBefore = () => {
     let tree = getTree()
     
-    tree.getNode(['root', 0, 0]).add.before(node('a new node'))
+    tree.getNode(['root', 0, 0]).add.before( node('a new node') )
 
     expect(
         tree.getNode(['root', 0, 0]).node.props.key
@@ -35,16 +33,17 @@ let addBefore = () => {
 let addInside = () => {
     let tree = getTree()
     
-    tree.getNode(['root', 0]).add.inside( node('a new node'), 1 )
+    tree.getNode(['root', 0]).add.inside( node('a new node'), 2 )
 
     expect(
-        tree.getNode(['root', 0, 1]).node.props.key
+        tree.getNode( ['root', 0, 2] ).node.props.key
     ).toBe('a new node')
 }
+
 let addAfter = () => {
     let tree = getTree()
     
-    tree.getNode(['root', 0, 0]).add.after(node('a new node'))
+    tree.getNode(['root', 0, 0]).add.after( node('a new node') )
 
     expect(
         tree.getNode(['root', 0, 1]).node.props.key
@@ -55,5 +54,5 @@ test('Add After', addAfter)
 test('Add Before', addBefore)
 test('Add Inside', addInside)
 
-test('Remove Self', removeSelf)
-test('Clone Self', cloneSelf)
+test('Remove', remove)
+test('Clone', clone)

6d81d252e0a630def0aee4bd9d62214bbc3c57b3
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-24 05:52:47 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/tests/unit/_move.spec.ts
+++ plugins/pockets/vue/dev/src/node-tree/tests/unit/_move.spec.ts
@@ -1,28 +1,43 @@
 import { testMove } from "./shared"
 
-test('Move Child Before Parent', () => {
+test('Child Before Target', () => {
     let { expectNode } = testMove('root.0', 'root.0.0', 'before')
     expectNode('root.0').toBe('root.0.0')
     expectNode('root.1.0').toBe('root.0.1')
 } )
 
-// test('Move Sibling Before Target', () => {
-//     let { expectNode } = testMove('root.0.0', 'root.0.2', 'before')
-//     expectNode('root.0.0', 'root.0.2')
-//     expectNode('root.0.1', 'root.0.0')
-//     expectNode('root.0.2', 'root.0.1')
-// } )
+test('Child After Target', () => {
+    let { expectNode } = testMove('root.0', 'root.0.0', 'after')
+    expectNode('root.0').toBe('root.0')
+    expectNode('root.1').toBe('root.0.0')
+} )
 
-// test('Try and move A node inside itself', () => {
-//     let { dropApi, expectNode } = testMove('root.0.0', 'root.0', 'inside')
+test('Sibling left of Target', () => {
+    let { expectNode } = testMove('root.0.0', 'root.0.2', 'before')
+    expectNode('root.0.0').toBe('root.0.2')
+    expectNode('root.0.1').toBe('root.0.0')
+} )
+
+test('Sibling Right of Target', () => {
+    let { expectNode } = testMove('root.0.0', 'root.0.2', 'after')
+    expectNode('root.0.0').toBe('root.0.0')
+    expectNode('root.0.1').toBe('root.0.2')
+} )
+
+
+
+
+
+test('Try and move A node inside itself', () => {
+    let { dropApi, expectNode } = testMove('root.0.0', 'root.0', 'inside')
     
-//     expectNode('root.0.0', 'root.01.0')
-//     expect(dropApi.inside).toBe(false)
-// })
+    expectNode('root.0.0').toBe( 'root.0.0')
+    expect(dropApi.inside).toBe(false)
+})
 
-// test('Target and Selected are the same', () => {
-//     let { dropApi } = testMove('root.0.0', 'root.0.0')
-//     expect(dropApi.after).toBe(false)
-//     expect(dropApi.inside).toBe(false)
-//     expect(dropApi.before).toBe(false)
-// })
+test('Target and Selected are the same', () => {
+    let { dropApi } = testMove('root.0.0', 'root.0.0')
+    expect(dropApi.after).toBe(false)
+    expect(dropApi.inside).toBe(false)
+    expect(dropApi.before).toBe(false)
+})

602617ed0f1d4c05022eff3f22fa48bc34fe35a4
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-02-26 07:39:33 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/tests/unit/_move-inside.spec.ts
+++ plugins/pockets/vue/dev/src/node-tree/tests/unit/_move-inside.spec.ts
@@ -2,6 +2,11 @@ import { testMove } from "./shared"
 
 describe('Move Inside', () => {
     test('Child of another parent', () => {
+        let { to } = testMove('root.2', 'root.0', 'inside')
+        to.test.node('root.1', 'root.2')
+    })
+
+    test('Child of another parent', () => {
         let { to } = testMove('root.0', 'root.1.1', 'inside')
         to.test.nodes('root.0', ['root.1.1', 'root.0.0', 'root.0.1', 'root.0.2', 'root.0.3'])
     })

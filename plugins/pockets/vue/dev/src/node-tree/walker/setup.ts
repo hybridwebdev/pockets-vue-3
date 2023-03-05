ac52ceb0bad52d2e1f24d6a42abb0fed159a8bfe
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-05 07:44:58 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/walker/setup.ts
+++ plugins/pockets/vue/dev/src/node-tree/walker/setup.ts
@@ -1,10 +1,21 @@
 import { editor } from "@/node-tree/editor"
-import { computed, ref, reactive } from "vue"
+import { computed, ref, reactive, provide, inject } from "vue"
 import { useInject } from "@/node-tree/create-tree-api/injection-key"
-
 export let setup = (props) => {
 
+    let createHiearchy = (api) => {
+        let providerKey = 'node-tree/hiearchy'
+        let hiearchy = inject(providerKey, [])
+        let newHiearchy = [...hiearchy, api]
+        provide(providerKey, newHiearchy)
+        return {
+            title: computed(() => newHiearchy.map(e => e.schema.title).join(" > "))
+        }
+    }
+
     let newApi = useInject().getNodeApi(props.treeNode)
+
+    let hiearchy = createHiearchy(newApi)
 
     let hovered = ref(false)
 
@@ -31,10 +42,8 @@ export let setup = (props) => {
 
     let toolTip = computed(() => {
 
-        let content = 'hello world'
-        
         return { 
-            content, 
+            content: hiearchy.title, 
             shown: editor?.show && state.hovered, 
             triggers: [], 
             placement: "auto-start", 

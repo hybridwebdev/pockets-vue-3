1070ad56e885bc028f3eef894e11e9a9ef999162
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-08 00:59:13 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/editor/modes/drag-drop/copy.vue
+++ plugins/pockets/vue/dev/src/node-tree/editor/modes/drag-drop/copy.vue
@@ -1,9 +1,70 @@
-<template>
-    <div class='p-2 fw-8 text-center'>   
-        I need to be coded
-    </div>
+<template lang='pug'>
+
+div(
+    class='p-2'
+)
+    div( 
+        v-if='!editor.active'
+        class='text-center fw-8'
+    ) 
+        p(
+            v-if='!selected'
+            class='m-0'
+        ) Select a node to copy.
+        p(
+            v-if='selected'
+        ) Select a target to copy node to.
+    div( 
+        v-if='editor.active' 
+    )
+        p( 
+            v-if='!editor.active.parent && !selected'
+            class='text-danger fw-8 text-center m-0' 
+        ) This Node cannot be copied!
+        div( 
+            v-if='!selected && editor.active.parent'
+            class='grid columns-1 gap-2'
+        )
+            p(
+                class='text-center fw-8 m-0'
+            ) Are you sure you want to copy this item?
+            button( 
+                @click='selectNode' 
+                class='btn btn-accent-dk mx-auto px-8 py-2 fw-8 text-uppercase'
+            ) Copy
+        div( 
+            v-if='selected' 
+        )
+            p(
+                class='text-center fw-8'
+            ) Select a location to copy selected node to.
+            
+            div(
+                class='grid columns-3 gap-2 mb-2'
+            )
+                nodePlacement( 
+                    :api='adder'
+                    @before="confirm('before')"
+                    @after="confirm('after')"
+                    @inside="confirm('inside')"
+                )
+    
+    div(
+        v-if='selected' 
+        class='d-flex'
+    )
+        button( 
+            @click='cancel' 
+            class='btn btn-danger mx-auto text-white fw-8 text-uppercase px-8 py-2'
+        ) Cancel
+        
+
 </template>
-<script>
+<script lang='ts'>
+import { setup } from "./setup"
+import nodePlacement from "@/node-tree/editor/sections/node-placement/index.vue"
 export default {
+    components: { nodePlacement },
+    setup: () => setup( { removeSelected: false } ) ,
 }
 </script>

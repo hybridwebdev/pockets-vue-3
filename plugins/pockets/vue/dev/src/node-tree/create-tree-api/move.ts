d3a55e53ba5087352285c52667b6518fca1ce8b6
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-08 05:44:06 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/create-tree-api/move.ts
+++ plugins/pockets/vue/dev/src/node-tree/create-tree-api/move.ts
@@ -3,7 +3,10 @@ import type { move, TreeNodeApi } from "@/node-tree/types"
 
 export let useMove = (api:TreeNodeApi) : move => {
     
-    let self = (to: number) => api.parent.move.child(api.paths.index, to )
+    let self = (to: number) => {
+        if(!api.parent) return;
+        return api.parent.move.child(api.paths.index, to )
+    }
     
     let child = (from: number, to: number) : false => {
         // api.node.nodes = $pockets.utils.array.move(api.node.nodes, from, to)

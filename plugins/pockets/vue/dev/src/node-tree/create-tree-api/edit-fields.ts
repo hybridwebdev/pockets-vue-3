deb1e3b982550e2edb57d1788f636af4e2272187
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-08 05:32:27 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/create-tree-api/edit-fields.ts
+++ plugins/pockets/vue/dev/src/node-tree/create-tree-api/edit-fields.ts
@@ -5,6 +5,7 @@ export let useEditFields = (api:TreeNodeApi) : Array<TreeNodeSchemaField> => {
     if(!api.schema || !api.node || !api.editor.nodes.fields) return []
     
     return api.editor.nodes.fields.filter((field: TreeNodeSchemaField) => {
+        if(!api.schema) return;
         return api.schema.fields.includes(field.ID)
     })
 

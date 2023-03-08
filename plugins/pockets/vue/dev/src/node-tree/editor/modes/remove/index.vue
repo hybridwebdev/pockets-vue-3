61833330ae7b158767f99fbceedf6180deb8a37f
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-08 08:01:16 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/editor/modes/remove/index.vue
+++ plugins/pockets/vue/dev/src/node-tree/editor/modes/remove/index.vue
@@ -33,19 +33,42 @@ import { treeNode } from "@/node-tree/types"
 
 let remove = () => {
 
-    let index = editor.active.paths.index
-    let length = editor.active.parent.node.nodes.length
     var newTarget: treeNode | null;
-    if(length==0) {
+
+    /**
+        First get index BEFORE removing.
+    */
+    
+    let index = editor.active.paths.index
+
+    editor.active.remove.self()
+    
+    /**
+        Logic to decide what new node should be
+    */
+
+    let length = editor.active.parent.node.nodes.length
+
+    if(length == 0) {
         newTarget = editor.active.parent.node
     }
-    if(length!=0) {
-        console.log(editor.active.paths.prevIndex)
+
+    if(length != 0) {
+        
+        if(index > length -1) {
+            /**
+                Nuding index to previous node as index is now out of bounds due to removal.
+            */
+            index--
+        }
+
+        newTarget = editor.active.parent.node.nodes[index]
+
     }
 
-    // editor.active.remove.self()
     if(newTarget) {
         editor.active = editor.active.getNodeApi(newTarget)
     }
+
 }
 </script>

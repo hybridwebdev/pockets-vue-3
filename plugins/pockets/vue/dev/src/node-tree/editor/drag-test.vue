c63166819eb6b4ecba359ea1b5877afb0a9301ab
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-09 09:22:20 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/editor/drag-test.vue
+++ plugins/pockets/vue/dev/src/node-tree/editor/drag-test.vue
@@ -3,69 +3,80 @@
     <slot v-bind='drag' :style='style'></slot>
 </template>
 <script setup lang="ts">
-import { ref, watch, computed } from 'vue'
+import { ref, watch, computed, toRefs } from 'vue'
 import { useDraggable, useElementSize, useWindowSize } from '@vueuse/core'
 
 type propDef = {
-    x: "start" | "end",
-    y: "start" | "end"
+    x?: "start" | "end",
+    y?: "start" | "end"
 }
 
 let props = withDefaults( defineProps<propDef>(), {
   x: 'end',
   y: "end"
-})
+} )
 
-let container = ref<HTMLElement | null>(null)
-let handle = ref<HTMLElement | null>(null)
+let useDraggableWithBoundary = (props) => {
 
-let elSize = useElementSize(container)
-let windowSize = useWindowSize()
+    let container = ref<HTMLElement | null>(null)
+    let handle = ref<HTMLElement | null>(null)
 
-let boundaries = computed( () => {
-    return {
-        x: {
-            start: 0,
-            end: windowSize.width.value - elSize.width.value
+    let elSize = useElementSize(container)
+    let windowSize = useWindowSize()
+
+    let boundaries = computed( () => {
+        return {
+            x: {
+                start: 0,
+                end: windowSize.width.value - elSize.width.value
+            },
+            y: {
+                start: 0,
+                end: windowSize.height.value - elSize.height.value
+            }
+        }
+    } )
+
+    let { x, y, style } = useDraggable(container, {
+        handle,
+        initialValue: { 
+            x: boundaries.value.x[props.x], 
+            y: boundaries.value.y[props.y], 
         },
-        y: {
-            start: 0,
-            end: windowSize.height.value - elSize.height.value
+    } )
+
+    let validateY = () => {
+        if(y.value < boundaries.value.y.start ) {
+            y.value = boundaries.value.y.start
+        }
+        if(y.value > boundaries.value.y.end ) {
+            y.value = boundaries.value.y.end
         }
     }
-} )
+    let validateX = () => {
+        if(x.value < boundaries.value.x.start ) {
+            x.value = boundaries.value.x.start
+        }
+        if(x.value > boundaries.value.x.end ) {
+            x.value = boundaries.value.x.end
+        }
+    }
 
-let { x, y, style } = useDraggable(container, {
-    handle,
-    initialValue: { 
-        x: boundaries.value.x[props.x], 
-        y: boundaries.value.y[props.y], 
-    },
-} )
+    watch([ x, elSize.width ], validateX)
+    watch([ y, elSize.height ], validateY)
 
-let validateY = () => {
-    if(y.value < boundaries.value.y.start ) {
-        y.value = boundaries.value.y.start
-    }
-    if(y.value > boundaries.value.y.end ) {
-        y.value = boundaries.value.y.end
-    }
-}
-let validateX = () => {
-    if(x.value < boundaries.value.x.start ) {
-        x.value = boundaries.value.x.start
-    }
-    if(x.value > boundaries.value.x.end ) {
-        x.value = boundaries.value.x.end
-    }
+    return  {
+        container,
+        handle,
+        style
+    } 
+
 }
 
-watch([ y, elSize.height ], validateY)
-watch([ x, elSize.width ], validateX)
+let { container, handle, style } = useDraggableWithBoundary(props)
 
 let drag = {
-    container,
-    handle,
+    container, handle
 }
 
 </script>

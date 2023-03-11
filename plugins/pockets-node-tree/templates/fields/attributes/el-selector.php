b2546546188dc62e5312d2af66f0042b9bcb003d
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-11 00:06:05 -0600

    c

--- plugins/pockets-node-tree/templates/fields/attributes/el-selector.php
+++ plugins/pockets-node-tree/templates/fields/attributes/el-selector.php
@@ -1,9 +1,13 @@
 
-<div>
+<div v-if='schema.elTypes && schema.elTypes.length > 0'>
     <label>Element Type</label>
     <select v-model='node.el' class='form-control'>
         <option v-for='type in schema.elTypes' :value='type'>
             {{type}}
         </option>
     </select>
+</div>
+
+<div v-else>
+    SNAH
 </div>

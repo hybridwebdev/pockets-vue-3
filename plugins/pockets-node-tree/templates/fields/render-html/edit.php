3bd4dee9c5f61fd83736666019a18535d64f6e1e
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-11 02:33:55 -0600

    c

--- plugins/pockets-node-tree/templates/fields/render-html/edit.php
+++ plugins/pockets-node-tree/templates/fields/render-html/edit.php
@@ -1,4 +1,5 @@
 <div>
     <label>Content</label>
-    <textarea v-model='node.props.innerHTML' class='form-control' row='40'></textarea>
+    <textarea v-model='node.props.innerHTML' class='form-control' row='40'>
+    </textarea>
 </div>

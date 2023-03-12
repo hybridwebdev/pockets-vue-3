8f29511d990e99245e68b9cb43a0bfc6c76ba9a2
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-12 01:10:06 -0600

    c

---
+++ plugins/pockets/vue/dev/src/components/UX/wysiwyg/pockets-plugin.ts
@@ -0,0 +1,6 @@
+import { defineAsyncComponent } from "vue"
+let component = defineAsyncComponent( () => import("./index.vue") )
+export let plugin:$pocketsPlugin = {
+    createApp: (app: any, $pockets: $pockets) => app.component('pockets-wysiwyg', component),
+}
+ 

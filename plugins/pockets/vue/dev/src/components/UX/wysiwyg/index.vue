9e5ec80b5aeee0e939cdf3acb04deaae06bc3d9a
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-12 02:12:37 -0600

    c

--- plugins/pockets/vue/dev/src/components/UX/wysiwyg/index.vue
+++ plugins/pockets/vue/dev/src/components/UX/wysiwyg/index.vue
@@ -1,13 +1,41 @@
-<template>
-  <editor-content v-if='editor' :editor="editor" class='bg-grey-lt'/>
+<template lang='pug'>
+div( 
+  v-if='editor'
+  class='border border-2 border-accent-dk'
+)
+  div(
+    class='d-flex bg-accent-dk gap-0'
+  )
+    button( 
+      class='btn btn-accent-dk'
+      @click="editor.chain().focus().toggleBold().run()"
+      :class="[ { 'active': editor.isActive('bold') }, btnClass ]"
+
+    )
+      i(class='fa fa-bold')
+
+    button( 
+      class='btn btn-accent-dk'
+      @click="editor.chain().focus().toggleItalic().run()"
+      :class="[ { 'active': editor.isActive('italic') }, btnClass ]"
+
+    )
+      i(class='fa fa-italic')
+      
+  editor-content( 
+    :editor="editor" 
+    class='editor-wrapper'
+  )
+
 </template>
 <script lang='ts' setup>
 import { onMounted, ref, onUnmounted } from "vue"
-
 import StarterKit from '@tiptap/starter-kit'
 import { Editor, EditorContent } from '@tiptap/vue-3'
 
 let emit = defineEmits(['update:modelValue'])
+
+let btnClass = 'rounded-0'
 
 let props = defineProps( {
     modelValue: {
@@ -26,12 +54,17 @@ let editorConfig = {
     onUpdate: () => emit( 'update:modelValue', editor.getHTML() ) ,
 }
 
-onMounted( () => {
-  editor = new Editor(editorConfig)
-} )
+onMounted( () => editor = new Editor(editorConfig) )
 
-onUnmounted( () => {
-  editor.destroy()
-} )
+onUnmounted( () => editor.destroy() )
 
 </script>
+<style lang='scss'>
+.editor-wrapper {
+  .ProseMirror {
+    padding: 10px;
+    outline: none;
+    background: var(--bs-grey-lt)
+  }
+}
+</style>

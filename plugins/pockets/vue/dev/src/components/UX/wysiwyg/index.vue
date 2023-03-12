37f5968d5349061da26659f6668905fac71c9ff7
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-12 01:45:57 -0600

    c

--- plugins/pockets/vue/dev/src/components/UX/wysiwyg/index.vue
+++ plugins/pockets/vue/dev/src/components/UX/wysiwyg/index.vue
@@ -1,56 +1,36 @@
 <template>
-  <editor-content :editor="editor" class='bg-grey-lt'/>
+  <editor-content v-if='editor' :editor="editor" class='bg-grey-lt'/>
 </template>
-
-<script>
+<script lang='ts' setup>
+import { onMounted, ref, onUnmounted } from "vue"
 import StarterKit from '@tiptap/starter-kit'
 import { Editor, EditorContent } from '@tiptap/vue-3'
 
-export default {
-  components: {
-    EditorContent,
-  },
+let emit = defineEmits(['update:modelValue'])
 
-  props: {
+let props = defineProps( {
     modelValue: {
       type: String,
-      default: '',
+      required: true
     },
-  },
+} )
 
-  emits: ['update:modelValue'],
+let editor = $ref(false)
 
-  data() {
-    return {
-      editor: null,
-    }
-  },
-
-  watch: {
-    modelValue(value) {
-
-      const isSame = this.editor.getHTML() === value
-
-      if (isSame) {
-        return
-      }
-
-      this.editor.commands.setContent(value, false)
-    },
-  },
-
-  mounted() {
-    this.editor = new Editor({
-      extensions: [
-        StarterKit,
-      ],
-      content: this.modelValue,
-      onUpdate: () => this.$emit( 'update:modelValue', this.editor.getHTML() ) ,
-    })
-  },
-
-  beforeUnmount() {
-    this.editor.destroy()
-  },
+let editorConfig = {
+    extensions: [
+      StarterKit,
+    ],
+    content: props.modelValue,
+    onUpdate: () => emit( 'update:modelValue', editor.getHTML() ) ,
 }
+
+onMounted( () => {
+  editor = new Editor(editorConfig)
+} )
+
+onUnmounted( () => {
+  editor.destroy()
+} )
+
 </script>

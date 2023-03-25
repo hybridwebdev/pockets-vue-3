0e1289bcd4527fbcbaa224e6fd9d67791ff3d8f0
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-25 09:17:57 -0600

    c

--- plugins/pockets/vue/dev/src/components/UX/tip-tap/nodes/wrapper/component.vue
+++ plugins/pockets/vue/dev/src/components/UX/tip-tap/nodes/wrapper/component.vue
@@ -1,5 +1,8 @@
 <template>
-    <node-view-wrapper as='p' @click='test'/>
+    <node-view-wrapper as='p' contenteditable='false'>
+      <node-view-content class="content" />
+      {{selected}} selected
+    </node-view-wrapper>
 </template>
 <script>
 import { NodeViewContent, nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'

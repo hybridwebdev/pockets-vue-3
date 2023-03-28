21e9cf14a31db55800ae50c625b50311aafb85a4
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-27 23:32:16 -0600

    c

--- plugins/pockets/vue/dev/src/components/UX/tip-tap/nodes/index.ts
+++ plugins/pockets/vue/dev/src/components/UX/tip-tap/nodes/index.ts
@@ -1,11 +1,6 @@
 import wrapper from "./wrapper"
-
 import { Paragraph } from "@tiptap/extension-paragraph"
-import { Document } from '@tiptap/extension-document'
-import { Text } from '@tiptap/extension-text'
 
 export let nodes = [
     Paragraph.extend(wrapper),
-    Document,
-    Text,
 ] 

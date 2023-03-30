c6e8d60a28ac894fcaa611cfdc95459d0719f1e0
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-03-30 05:14:28 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/extensions/nodes/wrapper/component.vue
+++ plugins/pockets/vue/dev/src/tip-tap/extensions/nodes/wrapper/component.vue
@@ -1,38 +1,15 @@
 <template>
     <node-view-wrapper 
         v-bind='{...node.attrs, ...node.type.spec.wrapperAttrs }'
-        @click='click'
     />
 </template>
 <script lang='ts'>
 import { nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'
-import { reactive, computed, watch } from 'vue'
-let setup = (props) => {
-    let click = () => {
-
-        let { updateAttributes, node, editor } = props
-
-        let attrs = new Proxy( node.attrs, {
-            get: (target, k) => target[k],
-            set: (target, k, v) => updateAttributes( { [k]:v } ) ? true : true
-        } )
-
-        editor.nodeTree.active = {
-            attrs,
-        }
-
-    }
-    
-    return { 
-        click
-    }
-}
 
 export default {
     components: {
         NodeViewWrapper
     },
-    setup, 
     props: nodeViewProps  
 }
 </script>

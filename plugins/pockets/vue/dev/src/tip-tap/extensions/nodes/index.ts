09bccd9138c56ec913847acf1ef8daf96ab1a3c2
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-04 06:37:49 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/extensions/nodes/index.ts
+++ plugins/pockets/vue/dev/src/tip-tap/extensions/nodes/index.ts
@@ -5,16 +5,9 @@ import Text from "./text"
 import Container from "./container"
 import base from "./base"
 import { Extension } from "@tiptap/core"
-import { uniqueId } from "lodash"
 
-let t = Extension.create({
-    name: "snah",
-    onTransaction(o) {
-        console.log(o)
-    },
-})
 export default [
-    t,
+
     Paragraph.extend(base),
     Image.extend(base),
     Container.extend(base),

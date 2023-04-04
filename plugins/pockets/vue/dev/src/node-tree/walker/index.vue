5a37d036da25da119889c7cad6228e4b8c7b4fe4
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-04 03:18:12 -0600

    c

--- plugins/pockets/vue/dev/src/node-tree/walker/index.vue
+++ plugins/pockets/vue/dev/src/node-tree/walker/index.vue
@@ -2,6 +2,12 @@
     <component 
         :is='treeNode.el'
         v-bind='treeNode.props'
+        class='pockets-node-tree-node'
+        @click.stop='clickHandler'
+        :class='classes'
+        @mouseenter='hoverHandler(true)'
+        @mouseleave='hoverHandler(false)'
+        v-tooltip='toolTip'
     >
         <component 
             v-for='(node, i) in treeNode.nodes'

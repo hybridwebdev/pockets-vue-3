62c37a95b9f9e068f330d0f5012aec8cf38eaca4
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-07 05:57:04 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/tool-bar/bubble-menu/component.ts
+++ plugins/pockets/vue/dev/src/tip-tap/tool-bar/bubble-menu/component.ts
@@ -1,4 +1,4 @@
-import { BubbleMenuPlugin, BubbleMenuPluginProps } from '@tiptap/extension-bubble-menu'
+import { BubbleMenuPlugin, BubbleMenuPluginProps } from './base'
 import {
   defineComponent,
   h,

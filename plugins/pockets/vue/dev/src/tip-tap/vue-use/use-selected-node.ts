245b742585ac20a3a6c6100977a8a94472027199
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 05:19:45 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/vue-use/use-selected-node.ts
+++ plugins/pockets/vue/dev/src/tip-tap/vue-use/use-selected-node.ts
@@ -1,43 +1,51 @@
-
+import type { Editor } from '@tiptap/core'
 import { 
   ref,
   onMounted,
   onUnmounted,
 } from "vue"
 
-export let useSelectedNode = (editor) => {
+export let useSelectedNode = (editor: Editor) => {
+  
   let nodeRef = ref(false)
+
+  let updateHandler = (view) => nodeRef.value = getSelectedNode(editor) 
+
+  onMounted( () => editor.on('transaction', updateHandler ) )
+
+  onUnmounted( () => editor.off('transaction', updateHandler ) )
+
   return nodeRef
 }
-export let helper = ( editor ) => {
+export let getSelectedNode = ( editor ) => {
 
-
-  let { selection } = editor.state
-  let { view } = editor
-  let node;
-  let pos;
+  return false;
+  // let { selection } = editor.state
+  // let { view } = editor
+  // let node;
+  // let pos;
   
-  if(selection.$from.depth === 0) {
-    editor.nodeTree.active = false
-  }
+  // if(selection.$from.depth === 0) {
+  //   editor.nodeTree.active = false
+  // }
 
-  if(!selection.node) {
-    node = selection.$head.parent
-    pos = selection.$from.before(selection.$from.depth)
-  }
+  // if(!selection.node) {
+  //   node = selection.$head.parent
+  //   pos = selection.$from.before(selection.$from.depth)
+  // }
   
-  if(selection.node) {
-    node = selection.node
-    pos = selection.$anchor.pos
-  }
+  // if(selection.node) {
+  //   node = selection.node
+  //   pos = selection.$anchor.pos
+  // }
 
-  let props = new Proxy(node.attrs, {
-    set: (target, key, value) => {
-      view.dispatch( view.state.tr.setNodeAttribute(pos, key, value) )
-      target[key] = value
-      return true
-    },
-    get: (target, key) =>  target[key],
-  })
+  // let props = new Proxy(node.attrs, {
+  //   set: (target, key, value) => {
+  //     view.dispatch( view.state.tr.setNodeAttribute(pos, key, value) )
+  //     target[key] = value
+  //     return true
+  //   },
+  //   get: (target, key) =>  target[key],
+  // })
 
 }

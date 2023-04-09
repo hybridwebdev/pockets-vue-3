fa23eb4999dc11c009618eba48fc9de53a7dfcd5
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 06:00:00 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/tool-bar/menu.vue
+++ plugins/pockets/vue/dev/src/tip-tap/tool-bar/menu.vue
@@ -6,7 +6,6 @@ bubble-menu()
   format-buttons() 
 </template>
 <script lang='ts' setup>
-import nodeInterface from "./node-interface"
 import formatButtons from "./format-buttons"
 import elementSelector from "./element-selector"
 import fontColor from "./font-color"

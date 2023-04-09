fc0f4e1eb28f632711b26cdc209e03d19448589f
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 04:01:53 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/node-tree.ts
+++
@@ -1,8 +0,0 @@
-import { EditorSchema } from "@/tip-tap/types"
-import { reactive } from "vue"
-export let nodeTree = reactive<EditorSchema>({
-  nodes: {},
-  options: {},
-  active: false,
-  inMenu: false
-})

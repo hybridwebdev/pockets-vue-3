8ef1de211dbd40fb6c0237d3dbed2307c7c481cb
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 05:57:08 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/tool-bar/menu.vue
+++ plugins/pockets/vue/dev/src/tip-tap/tool-bar/menu.vue
@@ -12,6 +12,6 @@ import elementSelector from "./element-selector"
 import fontColor from "./font-color"
 import fontFamily from "./font-family"
 
-import bubbleMenu from "./bubble-menu/index.vue"
+import bubbleMenu from "./bubble-menu/test.vue"
 </script>
 

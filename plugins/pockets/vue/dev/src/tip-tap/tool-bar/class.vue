114053da5557c727f00bab3522d4a37b9a997d31
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 06:08:03 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/tool-bar/class.vue
+++ plugins/pockets/vue/dev/src/tip-tap/tool-bar/class.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
-        CLASS
+    <div v-if='node'>
+        <input v-model='node.props.class'>
     </div>
 </template>
-<script>
-export default {
-}
+<script lang='ts' setup>
+import { useSelectedNode } from "@/tip-tap/vue-use"
+import { injectEditor } from "@/tip-tap/"
+let node = useSelectedNode(injectEditor())
 </script>

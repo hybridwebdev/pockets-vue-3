d2d8dd75a9f3a9b7f7f8db5eaeb56602a4432f6e
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 06:02:56 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/tool-bar/menu.vue
+++ plugins/pockets/vue/dev/src/tip-tap/tool-bar/menu.vue
@@ -4,13 +4,14 @@ bubble-menu()
   fontFamily()
   element-selector()
   format-buttons() 
+  classField()
 </template>
 <script lang='ts' setup>
 import formatButtons from "./format-buttons"
 import elementSelector from "./element-selector"
 import fontColor from "./font-color"
 import fontFamily from "./font-family"
-
+import classField from "./class"
 import bubbleMenu from "./bubble-menu/test.vue"
 </script>
 

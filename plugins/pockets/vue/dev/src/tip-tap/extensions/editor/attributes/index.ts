2cbd4240c25aa2615ba0791bb14e164403fd5be5
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 05:45:52 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/extensions/editor/attributes/index.ts
+++ plugins/pockets/vue/dev/src/tip-tap/extensions/editor/attributes/index.ts
@@ -1,5 +1,27 @@
 import '@tiptap/extension-text-style'
 import { Extension } from '@tiptap/core'
-export default Extension.create({
+export default Extension.create( {
     
-})
+    addOptions(){
+        return {
+            types: [
+                'heading',
+                'paragraph',
+            ]
+        }
+    },
+    
+    addGlobalAttributes(){
+        return [
+            {
+                types: this.options.types,
+                attributes: {
+                    class: {
+                        default: '',
+                    },
+                },
+            },
+        ]
+    }
+
+} )

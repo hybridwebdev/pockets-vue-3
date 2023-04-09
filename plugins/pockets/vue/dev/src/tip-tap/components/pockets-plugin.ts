6c47900c246466bc59da9b0a5b2390a0140e797d
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2023-04-09 06:48:16 -0600

    c

--- plugins/pockets/vue/dev/src/tip-tap/components/pockets-plugin.ts
+++ plugins/pockets/vue/dev/src/tip-tap/components/pockets-plugin.ts
@@ -1,6 +1,6 @@
 import { defineAsyncComponent } from "vue"
-let editor = defineAsyncComponent( () => import("./components/editor/index.vue") )
-let pocketsHtml = defineAsyncComponent( () => import("./components/pockets-html/index.vue") )
+let editor = defineAsyncComponent( () => import("./editor/index.vue") )
+let pocketsHtml = defineAsyncComponent( () => import("./pockets-html/index.vue") )
 export let plugin:$pocketsPlugin = {
     createApp: (app: any, $pockets: $pockets) => {
         app.component('pockets-wysiwyg', editor)
